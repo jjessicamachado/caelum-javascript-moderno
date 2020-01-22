8070f4772cc3dc6c8894701ff831bf717cb8351c
Jessica <jessicasmachado@live.com>
2020-01-22 16:07:48 -0300

    Colocar cartoes com jquery

--- js/formNovoCartao.js
+++ js/formNovoCartao.js
@@ -1,4 +1,5 @@
 const form = document.querySelector('.formNovoCartao');
+let numeroDoCartao = 0;
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -11,13 +12,47 @@ form.addEventListener('submit', function(e) {
         //aqui ele ainda nao foi inserido no DOM
 
         const btnSubmit = document.querySelector('.formNovoCartao-salvar')
-         
+
         form.addEventListener('animationend', function(e) { //animationend identifica o elemento em animaçao
             e.target.remove()
         })
 
-        form.insertBefore(msg, btnSubmit) //a mensagem tem q ser chamada antes do btnSubmit
+        form.insertBefore(msg, btnSubmit) //a mensagem tem q ser chamada antes do btnSubmit, aqui ele insere no DOM
+    } else {
+        numeroDoCartao++
+        const conteudo = textarea.value
+
+        const cartao = $(`<article id="cartao_${numeroDoCartao}" class="cartao" tabindex="0">
+        <div class="opcoesDoCartao">
+          <button class="opcoesDoCartao-remove opcoesDoCartao-opcao" tabindex="0">
+            <svg><use xlink:href="#iconeRemover"></use></svg>
+          </button>
+
+          <input type="radio" name="corDoCartao${numeroDoCartao}" value="#EBEF40" id="corPadrão-cartao${numeroDoCartao}" class="opcoesDoCartao-radioTipo" checked>
+          <label for="corPadrão-cartao3" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #EBEF40;" tabindex="0">
+            Padrão
+          </label>
+
+          <input type="radio" name="corDoCartao${numeroDoCartao}" value="#F05450" id="corImportante-cartao${numeroDoCartao}" class="opcoesDoCartao-radioTipo">
+          <label for="corImportante-cartao${numeroDoCartao}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #F05450;" tabindex="0">
+            Importante
+          </label>
+
+          <input type="radio" name="corDoCartao${numeroDoCartao}" value="#92C4EC" id="corTarefa-cartao${numeroDoCartao}" class="opcoesDoCartao-radioTipo">
+          <label for="corTarefa-cartao${numeroDoCartao}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #92C4EC;" tabindex="0">
+            Tarefa
+          </label>
+
+          <input type="radio" name="corDoCartao${numeroDoCartao}" value="#76EF40" id="corInspiração-cartao${numeroDoCartao}" class="opcoesDoCartao-radioTipo">
+          <label for="corInspiração-cartao${numeroDoCartao}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #76EF40;" tabindex="0">
+            Inspiração
+          </label>
+        </div>
+        <p class="cartao-conteudo" contenteditable tabindex="0">${conteudo}</p>
+      </article>`)
+      $('.mural').append(cartao)
     }
+    
 })
 
 form.classList.remove('no-js');
