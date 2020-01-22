a5cd70e2fa69a1766bdaaeedeaff51adcb83ab68
Jessica <jessicasmachado@live.com>
2020-01-22 11:01:57 -0300

    Botao de remover no cartao js

--- js/cartao.js
+++ js/cartao.js
@@ -1,10 +1,11 @@
 const cartao = document.querySelectorAll('.cartao');
 
 for (let i = 0; i < cartao.length; i++) {
-    cartao[i].addEventListener('focusin', function() {
-        cartao[i].classList.add('cartao--focado'); }
-    ) 
-    cartao[i].addEventListener('focusout', function() {
+    cartao[i].addEventListener('focusin', function () {
+        cartao[i].classList.add('cartao--focado');
+    }
+    )
+    cartao[i].addEventListener('focusout', function () {
         cartao[i].classList.remove('cartao--focado');
     })
 
@@ -17,6 +18,16 @@ for (let i = 0; i < cartao.length; i++) {
     cartao[i].addEventListener('keydown', function (e) {
         if (e.target.classList.contains('opcoesDoCartao-opcao') && (e.key === 'Enter')) {
             e.target.click()
+        }
+    })
+
+    cartao[i].addEventListener('click', function (e) {
+        if (e.target.classList.contains('opcoesDoCartao-remove')) {
+            cartao[i].classList.add('cartao--some')
+
+            cartao[i].addEventListener('transitionend', function () {
+                cartao[i].remove();
+            })
         }
     })
 }
