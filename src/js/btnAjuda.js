4ef47cb3a6f25a03c4a944e0575240af79f3e3b1
Jessica <jessicasmachado@live.com>
2020-01-23 14:50:13 -0300

    Requisiçao ajax pra buscar no servidor os cartoes de ajuda

--- src/js/btnAjuda.js
+++ src/js/btnAjuda.js
@@ -1,12 +1,25 @@
 const btnAjuda = document.querySelector('#btnAjuda')
 
 btnAjuda.addEventListener('click', function() {
-    const ajudas = [{conteudo:'Texto', cor: '#ca99e8'}, {conteudo:'Texto', cor: '#83ebe4'}, {conteudo:'Texto', cor: '#768df5'}]
+    // const ajudas = [{conteudo:'Texto', cor: '#ca99e8'}, {conteudo:'Texto', cor: '#83ebe4'}, {conteudo:'Texto', cor: '#768df5'}]
 
+    // ajudas.forEach(function (ajuda) {
+    //     adicionarCartao(ajuda)
+    // })
 
-    ajudas.forEach(function (ajuda) {
-        adicionarCartao(ajuda)
-    })
+        const xhr = new XMLHttpRequest();
+        xhr.open('get', 'https://ceep.herokuapp.com/cartoes/instrucoes')
+        xhr.responseType = "json"
+        xhr.send()
+
+        xhr.addEventListener('load', function() {
+            const objeto = xhr.response
+            const ajudas = objeto.instrucoes
+
+            ajudas.forEach(function (ajuda) {
+                adicionarCartao(ajuda)
+            })
+        })
 
 })
 
